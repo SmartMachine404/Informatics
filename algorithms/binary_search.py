0c619f26cd65ee66a0748926919faffa13bf883a
SmartMachine404 <smartmacine@yandex.com>
2020-09-18 22:58:05 +0300

    sorted

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -1,14 +1,12 @@
-def binary_search_in_range(minimum, maximum):
-	more = True
-	less = False
-	left = minimum
-	right = maximum
+def binary_search(element, arr):
+	left = 0
+	right = len(arr)
 	while right - left > 1:
-		middle = (right + left)//2 - 1
-		answer = int(input(f'Is your number more than {middle}? [{left} {right}) '))
-		if answer == more:
+		middle = (right + left) // 2
+		if element > arr[middle]:
 			left = middle + 1
+		elif element < arr[middle]:
+			right = middle
 		else:
-			right = middle + 1
-	print(f'Your number is {left}')
-binary_search_in_range(1, 120)
+			return middle
+	return left
