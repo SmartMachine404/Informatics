95efdf14574ab48338f80b73ac7b8ae7662a9b49
Aeek True <smartmachintgm@yandex.ru>
2020-12-13 10:12:57 +0300

    MUN20

--- 2020SCHOOL/d.py
+++ 2020SCHOOL/d.py
@@ -1,20 +1,34 @@
 n = int(input())
 sizes = [int(input()) for i in range(n)]
-p = [0] * n
-p[-1] = 1
 
-summ = sum(sizes)
-up = sizes[-1]
+def check(player):
+	if sizes[player] == sizes[0]:
+		return 0
+	s = sum(sizes[0:player+1])
+	for g in sizes[player+1::]:
+		if g < s:
+			s += g
+		else:
+			return 0
+	return 1
 
-for gamer in range(n-2, -1, -1):
-	if summ - up > sizes[gamer+1]:
-		p[gamer] = 1
-		up += sizes[gamer]
+if len(sizes) == 1:
+	p = 0
+elif sizes[-1] == sizes[0]:
+	p = n
+else:
+	last_loser = 0
+	first_winner = n-1
+	while first_winner - last_loser > 1:
+		mid = (first_winner + last_loser)//2
+		if check(mid):
+			first_winner = mid
+		else:
+			last_loser = mid
+	p = first_winner
+
+for i in range(n):
+	if i >= p:
+		print(1)
 	else:
-		break
-
-if p[-1] == p[0]:
-	p = [0]*n
-
-for i in p:
-	print(i)
+		print(0)
